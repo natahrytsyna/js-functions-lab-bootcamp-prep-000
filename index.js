66b5a87ec7445bb19383f670507d3cedb4bac2ea
Nataliia Hrytsyna <nataliia.hrytsyna@gmail.com>
2019-06-20 10:26:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy hollidays!"
+}
+
+function happyHolidaysTo(name) {
+  return 'Happy hollidays, ${name}!'
+}
+
+function happyHolidayTo(holliday, name) {
+  return 'Happy ${holliday}, ${name}!'
+}
+
+
